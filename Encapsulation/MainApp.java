00147e13f09995fba98bc2b489c1eb030b4a28b8
dnewfernando <dnewfernando@gmail.com>
2026-06-07 18:59:54 +0700

    Create MainApp.java

---
+++ Encapsulation/MainApp.java
@@ -0,0 +1,10 @@
+public class MainApp {
+    public static void main(String[] args){
+        RekeningBank rekeningSaya = new RekeningBank("12345", 1000000.0);
+        
+        rekeningSaya.setor(500000.0);
+        rekeningSaya.tarik(200000.0);
+
+        System.out.println("Sisa saldo di rekening: "+rekeningSaya.getSaldo());
+    }
+}
