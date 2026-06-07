a23ec7de37e94ec86da775d5ee01376c031a5842
dnewfernando <dnewfernando@gmail.com>
2026-06-07 19:00:11 +0700

    Create MainApp.java

---
+++ Inheritance/MainApp.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Kucing siManis = new Kucing("Si Manis");
+
+        siManis.makan();
+        siManis.bersuara();
+        siManis.panjatPohon();
+    }
+}
